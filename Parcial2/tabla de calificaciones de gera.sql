ba3ae2e62537003f6ea2fec960caa1b9bad812be
XxgeraxX5851 <gutierrez.gerardo.2024@cbtis-246.edu.mx>
2025-04-04 16:14:55 -0600

    Add files via upload

---
+++ Parcial2/tabla de calificaciones de gera.sql
@@ -0,0 +1,20 @@
+create table "clases de gera"  (
+	id INT DEFAULT null,
+	materia varchar (30) DEFAULT NULL,
+	alumno varchar (30) DEFAULT NULL,
+	calificacion INT DEFAULT NULL
+);
+
+--DROP TABLE calificaciones 
+
+INSERT INTO "clases de gera" (id ,materia,alumno,calificacion) VALUES
+(1,'conservacio de la energia y sus interaccion con la materia 2', 'Gerardo', 9),
+(2,'lenguaje y comunicacion 2','gerardo',10),
+(3,'pensamiento matematico 2','gerardo',7),
+(4,'ingles 2','gerardo',10),
+(5,'cultura digital 2','gerardo',10),
+(6,' ciencias sociales 2','gerardo',9),
+(7,'desarollo de sofware 2','gerardo',10),
+(8,'taller 2','gerardo',10),
+(9,'implemnetacion de sofware 2','gerardo',9),
+(10,'codificacion de spfware de sistemas informaticos 2','gerardo',9);
